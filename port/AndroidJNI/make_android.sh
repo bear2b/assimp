bd33463e42cb92ab0066df7b83540baaa3a89973
Mikhail <Mikhail@bear2b.com>
2019-07-24 16:28:13 +0300

    ndk version update

--- port/AndroidJNI/make_android.sh
+++ port/AndroidJNI/make_android.sh
@@ -1,9 +1,9 @@
 #CMAKE_PATH="C:\Program Files\CMake\bin\cmake.exe"
-ANDROID_NDK_PATH=~/ndk/android-ndk-r18b #/usr/local/Caskroom/android-ndk/18/android-ndk-18
+ANDROID_NDK_PATH=~/ndk/android-ndk-r20 #/usr/local/Caskroom/android-ndk/18/android-ndk-18
 ANDROID_CMAKE_PATH=${ANDROID_NDK_PATH}/build/cmake/android.toolchain.cmake  
 #cmakeontrib\android-cmake
 
-ANDROID_NDK=~/ndk/android-ndk-r18b #/usr/local/Caskroom/android-ndk/18/android-ndk-r18
+#ANDROID_NDK=~/ndk/android-ndk-r20 #/usr/local/Caskroom/android-ndk/18/android-ndk-r18
 
 #rmdir /s /q build
 mkdir build
@@ -21,7 +21,7 @@ CPP_STD=${CPP_STD_LIST[0]}
     export CPPFLAGS=$CFLAGS
     export CXXFLAGS="$CFLAGS -std=$CPP_STD"
 
-cmake -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake \
+cmake -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_PATH}/build/cmake/android.toolchain.cmake \
   -DANDROID_NATIVE_API_LEVEL=android-21 \
   -DASSIMP_BUILD_GLTF_IMPORTER=ON \
   -DASSIMP_BUILD_ASSIMP_TOOLS=OFF \
